8e12ed77bbc1facd570827d214738e51b50c42c3
Abhijeet Jha <jhaabhijeet864@gmail.com>
2026-03-22 18:47:52 +0530

    create: pattern_1 in java - abhijeet pacakge

---
+++ abhijeet/pattern_1.java
@@ -0,0 +1,20 @@
+package abhijeet;
+
+import java.util.Scanner;
+
+public class pattern_1
+{
+    public static void main(String args[])
+    {
+        /*
+        *
+        * *
+        * * *
+        * * * *
+         */
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
