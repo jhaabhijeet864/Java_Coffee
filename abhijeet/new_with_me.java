01922871759f797ed1dec887612e0284e6d096f9
Abhijeet Jha <jhaabhijeet864@gmail.com>
2026-03-22 18:41:26 +0530

    created package:abhijeet

---
+++ abhijeet/new_with_me.java
@@ -0,0 +1,16 @@
+package abhijeet;
+
+public class new_with_me {
+    public static void main(String[] args) {
+
+        /*
+        1. print -> prints in the same line
+        2. println  -> prints in the next line
+        3. "\n"  -> another method to print in the next line to the prior one
+        */
+        System.out.println("Hello World with java");
+        System.out.print("Hello World with me");
+        System.out.print("Hello World with me and java\n");
+
+    }
+}
