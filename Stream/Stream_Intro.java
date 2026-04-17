70d01cdaadf5d4a751529b31fd3aef113992363b
Abhijeet Jha <jhaabhijeet864@gmail.com>
2026-04-17 13:35:04 +0530

    Progress

---
+++ Stream/Stream_Intro.java
@@ -0,0 +1,15 @@
+package Stream;
+
+import java.util.*;
+
+
+public class Stream_Intro {
+    // Stream API is a way to express and process collections of objects
+    public static void main(String[] args) {
+        List<Integer> ll = Array.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        l.stream().m
+    }
+
+}
+
+
--- multithreading/multi_threading.java
+++ multithreading/multi_threading.java
@@ -1,4 +1,29 @@
 package multithreading;
 
+/*
+What is Thread?
+-> A thread is a single sequential flow of execution within a program
+-> Thread is a "Light Weight Process" -> because it uses less resources
+
+- Types of Threads
+1. main - high priority thread
+2. Daemon  - low priority thread (Priority : 1) -> Garbage Collector
+*  */
+
+/*
+ lIFE CYCEL OF THREAD
+ 1. Newborn state
+ 2. Runnable state
+ 3. Running state
+ 4. Blocked state
+ 5. Dead state
+ */
+
 public class multi_threading {
+    // A single hand handling multiple parts     of the program.
+    // Thread is a subset of process
+    fir (int i = 1; i <= 10; i++ ) {
+        System.out.print();
+    }
+
 }
