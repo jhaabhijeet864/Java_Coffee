e00e362dcb1832b0c3d2627c022643a796536afd
Abhijeet Jha <jhaabhijeet864@gmail.com>
2026-04-20 02:13:05 +0530

    tried writing animal class

--- Practical/Animal.java
+++ Practical/Animal.java
@@ -1,4 +1,19 @@
 package Practical;
 
-public class Animal {
+class Animal{
+    void sound(){
+        System.out.println("Animal sound method called");
+    }
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog sound method called");
+    }
+}
+class Override{
+    public static void main(String args[]){
+        Dog d=new Dog();
+        System.out.println("Object created for Dog class");
+        d.sound();
+    }
 }
